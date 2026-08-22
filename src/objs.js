12c5f4c7077784bf40e0d1ab9308c202d4d1c5a4
Tsunami014 <tsunami014@duck.com>
2026-08-22 14:28:36 +1000

    Split the objs into a separate file for the screens

--- src/objs.js
+++ src/objs.js
@@ -14,19 +14,3 @@ class TestObj {
         ]}
     }
 }
-
-const SCREENS = {
-    home: [
-        { labl: "Stage", open: true, conts: [
-            (new TestObj("Test")).screenObj()
-            { labl: "Hello", conts: [
-                { labl: "Obj", class: "image" },
-            ]},
-        ]},
-    ],
-    projects: [
-        { labl: "Stage", open: true, conts: [
-            { labl: "Test", class: "image" },
-        ]},
-    ],
-}
---
+++ src/screens.js
@@ -0,0 +1,15 @@
+const SCREENS = {
+    home: [
+        { labl: "Stage", open: true, conts: [
+            (new TestObj("Test")).screenObj()
+            { labl: "Hello", conts: [
+                { labl: "Obj", class: "image" },
+            ]},
+        ]},
+    ],
+    projects: [
+        { labl: "Stage", open: true, conts: [
+            { labl: "Test", class: "image" },
+        ]},
+    ],
+}
